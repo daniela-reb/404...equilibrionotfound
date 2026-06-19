7b3e16f4bfdf1bcc10fa7fd60bff030697f8e5b8
Daniela Rus <dana_cozmi@yahoo.co.uk>
2026-06-19 15:20:38 +0300

    agiunto vercel analytics

--- components/about/AboutCard.tsx
+++ components/about/AboutCard.tsx
@@ -5,7 +5,7 @@ export default function AboutCard({title, description}: AboutCardData) {
     return(
         <div className="flex flex-1 w-full max-w-md flex-col items-center gap-3 py-6 px-6 bg-white/20 rounded-2xl shadow-xl">
           
-            <h1 className="text-lg font-bold text-white tracking-wide">
+            <h1 className="text-lg font-bold text-black md:text-white tracking-wide">
                 {title}
                 </h1>
 
